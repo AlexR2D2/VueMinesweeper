263cc2fadbf370251e951bef5bd8c370305b014d
Александр Голубов <Alex@alex.local>
2018-02-15 19:25:21 +0500

    Update

--- components/Hello.vue
+++ components/Hello.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="pv3 ph6 shadow-4 tc">
-    Hello!<br/><span class="silver f4">I am Nuxt.js project.</span>
+    <div class="f2 pv2">
+      Hello
+    </div>
+    <div class="silver f4">
+      I am Nuxt.js project.
+    </div>
   </div>
 </template>
