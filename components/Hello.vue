34b787cd6d75f53237ba0c6f7c550a3c0d48842b
Александр Голубов <Alex@alex.local>
2018-02-15 18:44:05 +0500

    Initial commit

---
+++ components/Hello.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="pv3 ph6 shadow-4 tc">
+    Hello!<br/><span class="silver f4">I am Nuxt.js project.</span>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="flex justify-center pv7 f2">
+    <hello/>
+  </section>
+</template>
+
+<script>
+import Hello from '~/components/Hello.vue'
+
+export default {
+  layout: 'default',
+
+  components: {
+    'hello': Hello,
+  }
+}
+</script>
